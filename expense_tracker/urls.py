4cb60e400ab9e0e4e347f8caedae3ebc5e3266e1
JacklineMacharia <jacklinemacharia99@gmail.com>
2025-04-01 16:28:33 +0300

    Added views and URLs

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('expenses.urls')),
+    path('', include('expenses.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
